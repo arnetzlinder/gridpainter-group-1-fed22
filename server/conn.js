05408067395a564429b82be54c505283bd96bc8d
nikolajnowicki <114876157+nikolajnowicki@users.noreply.github.com>
2023-04-18 21:46:57 +0200

    Added Registration & Login Functionality

--- server/conn.js
+++ server/conn.js
@@ -1,14 +1,11 @@
 const mysql = require("mysql2");
 
 connection = mysql.createConnection({
- host: "localhost",
- port: "3306",
- user: "admin",
- password: "admin",
- database: "gridpainter",
+  host: "localhost",
+  port: "3306",
+  user: "gridpainter",
+  password: "gridpainter",
+  database: "gridpainters",
 });
-
-
-
 
 module.exports = connection;
--- server/index.js
+++ server/index.js
@@ -1,33 +1,44 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const mysql = require("mysql2");
-const connection = require("./conn");
+const express = require("express");
+const path = require("path");
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mysql = require("mysql2");
+const connection = require("./conn");
+require("dotenv").config();
 
+const registerRouter = require("./routes/register");
+const loginRouter = require("./routes/login");
 
-app.get('/', (req, res) => {
- res.send('Hej Socket server');
-})
+app.get("/", (req, res) => {
+  res.send("Hej Socket server");
+});
 
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"],
+  },
+});
 
-const io = require('socket.io')(server, {
-    cors: {
-    origin: 'http://127.0.0.1:5500',
-    methods: ['GET', 'POST']
- }
-})
+io.on("connection", (socket) => {
+  console.log("någon är här");
+  socket.on("chat", (arg) => {
+    console.log("incoming chat", arg);
+    io.emit("chat", arg);
+  });
+});
 
-io.on('connection', (socket) => {
-    console.log('någon är här')
-    socket.on('chat', (arg) => {
-    console.log('incoming chat', arg);
-    io.emit('chat', arg);
- })
+server.listen(3000);
 
-})
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-
-
-
-server.listen(3000)
+app.use("/register", registerRouter);
+app.use("/login", loginRouter);
