df9953d0d45bff6ae0d1ecd80acd4dcf7018d509
Veronica <veronica.bergstrom@medieinstitutet.se>
2023-04-24 20:36:26 +0200

    added event and socket for play again button

--- klient/js/pickcolor.js
+++ klient/js/pickcolor.js
@@ -4,91 +4,95 @@ const socket = io("http://localhost:3000");
 import { renderGameboard } from "./gameboard.js";
 import { renderGamechat } from "./gamechat.js";
 
-localStorage.removeItem('playerColor');
+localStorage.removeItem("playerColor");
 
-let cont = document.getElementById('pickColors');
+let cont = document.getElementById("pickColors");
 
-export function renderColors(){
+export function renderColors() {
   cont.innerHTML = `
   <button id="startBtn">Starta spel</button>
   `;
 
-  let startBtn = document.getElementById('startBtn');
+  let startBtn = document.getElementById("startBtn");
   let colorsArr = [];
 
-  startBtn.addEventListener('click', () => {
+  startBtn.addEventListener("click", () => {
     socket.emit("entering");
     renderGamechat();
-  })
+  });
 
   socket.on("entering", (arg) => {
-    let userColor = localStorage.getItem('playerColor');
-    if(userColor){
+    let userColor = localStorage.getItem("playerColor");
+    if (userColor) {
       let color;
-      if(userColor == 'red'){
-        color = 'röd'
-      } else if(userColor == 'yellow'){
-        color = 'gul';
-      } else if(userColor == 'brown'){
-        color = 'brun';
-      } else{
-        color = 'svart';
+      if (userColor == "red") {
+        color = "röd";
+      } else if (userColor == "yellow") {
+        color = "gul";
+      } else if (userColor == "brown") {
+        color = "brun";
+      } else {
+        color = "svart";
       }
       cont.innerHTML = `<p>Du har valt färgen ${color}</p>`;
-      renderGameboard();
-    } else{
+    } else {
       colorsArr = arg;
       renderButtons(colorsArr);
     }
-  })
+  });
 }
 
-function renderButtons(colorsArr){
-  console.log(colorsArr)
-  if (colorsArr.length < 1){
-    cont.innerHTML = 'Alla färger är tagna';
-    return
+export function renderButtons(colorsArr) {
+  console.log(colorsArr);
+  if (colorsArr.length < 1) {
+    cont.innerHTML = "Alla färger är tagna";
+    return;
   }
   cont.innerHTML = `
   <h2>Välj en färg:</h2>
   <div id="colorsCont"></div>
   `;
 
-  let colorsCont = document.getElementById('colorsCont')
+  let colorsCont = document.getElementById("colorsCont");
 
-  colorsArr.map(button => {
+  colorsArr.map((button) => {
     colorsCont.innerHTML += button;
-  })
+  });
 
-  let buttons = document.querySelectorAll('.colorBtn');
+  let buttons = document.querySelectorAll(".colorBtn");
 
-  buttons.forEach(button => {
-    button.addEventListener('click', (e) => {
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
       let pickedArray = [];
-      
+
       renderGameboard();
 
-        if(e.target.id == 'btn-0'){
-          localStorage.setItem('playerColor', 'red');
-          socket.emit("removeColor", {button: '<button id="btn-0" class="colorBtn">'});
-          renderButtons(pickedArray);
-  
-        } else if(e.target.id == 'btn-1'){
-          localStorage.setItem('playerColor', 'yellow');
-          socket.emit("removeColor", {button: '<button id="btn-1" class="colorBtn">'});
-  
-        } else if(e.target.id == 'btn-2'){
-          localStorage.setItem('playerColor', 'brown');
-          socket.emit("removeColor", {button: '<button id="btn-2" class="colorBtn">'});
-  
-        } else if(e.target.id == 'btn-3'){
-          localStorage.setItem('playerColor', 'black');
-          socket.emit("removeColor", {button: '<button id="btn-3" class="colorBtn">'});
-        } 
-    })
-  })
+      if (e.target.id == "btn-0") {
+        localStorage.setItem("playerColor", "red");
+        socket.emit("removeColor", {
+          button: '<button id="btn-0" class="colorBtn">',
+        });
+        renderButtons(pickedArray);
+      } else if (e.target.id == "btn-1") {
+        localStorage.setItem("playerColor", "yellow");
+        socket.emit("removeColor", {
+          button: '<button id="btn-1" class="colorBtn">',
+        });
+      } else if (e.target.id == "btn-2") {
+        localStorage.setItem("playerColor", "brown");
+        socket.emit("removeColor", {
+          button: '<button id="btn-2" class="colorBtn">',
+        });
+      } else if (e.target.id == "btn-3") {
+        localStorage.setItem("playerColor", "black");
+        socket.emit("removeColor", {
+          button: '<button id="btn-3" class="colorBtn">',
+        });
+      }
+    });
+  });
 }
 
-function hideColors(){
-  cont.innerHTML = '';
+function hideColors() {
+  cont.innerHTML = "";
 }
--- klient/js/startgame.js
+++ klient/js/startgame.js
@@ -1,6 +1,6 @@
 import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
+import { renderButtons } from "./pickcolor.js";
 const socket = io("http://localhost:3000");
-
 
 //start game button
 export function renderStartBtn() {
@@ -17,20 +17,13 @@ export function renderStartBtn() {
 //start game function
 async function startGame() {
   // plz start the game!
-  socket.emit('startgame')
-
-
-  // const image = await fetchImage();
-  // //store in play image
-  // await showPreview(image);
-  // console.log("loggas");
-  // //await call TimerFunction (will start after 5 sec)
+  socket.emit("startgame");
 }
 
 socket.on("startgame", (arg) => {
-  console.log("The server told us to strart the game with this image: ")
-  console.log(arg)
-  let image = arg["picture-array"]
+  console.log("The server told us to strart the game with this image: ");
+  console.log(arg);
+  let image = arg["picture-array"];
   for (let i = 0; i < image.length; i++) {
     const row = image[i];
     for (let j = 0; j < row.length; j++) {
@@ -40,64 +33,37 @@ socket.on("startgame", (arg) => {
       cell.classList.add(color);
     }
   }
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const cells = document.getElementsByClassName("cell");
-      [...cells].forEach((cell) => {
-        cell.classList.remove("brown", "black", "red", "yellow");
-        resolve();
-      });
-    }, 5000);
-  });
-})
+  setTimeout(() => {
+    const cells = document.getElementsByClassName("cell");
+    [...cells].forEach((cell) => {
+      cell.classList.remove("brown", "black", "red", "yellow");
+    });
+  }, 5000);
+});
 
 socket.on("endgame", (arg) => {
-  let gameContainer = document.getElementById('game');
-  console.log("The end is near!!!!111one")
-  console.log("Correctly painted percentage: "+arg)
-  arg = arg*100;
+  let gameContainer = document.getElementById("game");
+  console.log("The end is near!!!!111one");
+  console.log("Correctly painted percentage: " + arg);
+  arg = arg * 100;
 
   let percentage = Math.round(arg);
 
   gameContainer.innerHTML = `
   <h2>Ni fick ${percentage}% rätt</h2>
-  <button>Spela igen</button>`;
+  <button id="playAgainBtn">Spela igen</button>`;
+
+  //play again event
+  playAgainBtn.addEventListener("click", () => {
+    socket.emit("playAgain");
+  });
 
   //window.alert("Correctly painted percentage: "+arg+"%")
+});
 
-})
-//shows image for 5 secs and then clear board
-function showPreview(image) {
-  for (let i = 0; i < image.length; i++) {
-    const row = image[i];
-    for (let j = 0; j < row.length; j++) {
-      const color = row[j];
-      const cell = document.getElementById(`${i}-${j}`);
-      cell.classList.remove("brown", "black", "red", "yellow");
-      cell.classList.add(color);
-    }
-  }
-  //timeOut after 5 sec and clear board (is a promise for other functions)
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const cells = document.getElementsByClassName("cell");
-      [...cells].forEach((cell) => {
-        cell.classList.remove("brown", "black", "red", "yellow");
-        resolve();
-      });
-    }, 5000);
-  });
-}
-
-//now fetching a random picture from db for every click on button
-async function fetchImage() {
-  const response = await fetch("http://localhost:3000/images/random", {
-    headers: { "Content-Type": "application/json" },
-  });
-  if (!response.ok) {
-    alert("Could not load image, please try again");
-  } else {
-    const imageData = await response.json();
-    return imageData["picture-array"];
-  }
-}
+// play again listener
+socket.on("playAgain", (startBtns) => {
+  let gameContainer = document.getElementById("game");
+  gameContainer.innerHTML = "";
+  renderButtons(startBtns);
+});
