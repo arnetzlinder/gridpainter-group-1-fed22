ae2ba057bcec4643c1e2adae051a1b3ac58493ae
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-04-24 22:01:28 +0200

    Added online database instead of localhost

--- server/conn.js
+++ server/conn.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 connection = mysql.createConnection({
-  host: "localhost",
-  port: "*",
-  user: "gridpainters",
-  password: "gridpainters",
-  database: "gridpainters",
+  host: process.env.MYSQL_HOST,
+  port: "3306",
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
 module.exports = connection;
